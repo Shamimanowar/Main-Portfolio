260fc688f5b93671516357f60527a89b532d53c4
Shamim Anowar <shamim@bildnw.com>
2023-01-28 21:40:01 +0600

    styles are imported in the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/index.css";
+import "./styles/global.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
